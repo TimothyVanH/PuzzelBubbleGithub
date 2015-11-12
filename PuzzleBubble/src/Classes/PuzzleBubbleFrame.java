142c78a06dcb8ad301bfb014447eae846c51cfc7
Laurens De Paepe <laurens.depaepe@gmail.com>
2015-11-12 20:23:33 +0100

    Frame size, middle of screen

--- PuzzleBubble/src/Classes/PuzzleBubbleFrame.java
+++ PuzzleBubble/src/Classes/PuzzleBubbleFrame.java
@@ -1,6 +1,7 @@
 package Classes;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by Timothy on 30/10/2015.
@@ -8,20 +9,14 @@ import javax.swing.*;
 public class PuzzleBubbleFrame extends JFrame {
     public PuzzleBubbleFrame(PuzzleBubble puzzleBubble){
         super();
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+
         setTitle("PuzzleBubble");
         setContentPane(new PuzzleBubbleDrawingPanel(puzzleBubble));
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(new Dimension(800, 600));
+        setLocation(dim.width/2-getSize().width/2, dim.height/2-getSize().height/2);
+
         setVisible(true);
-
-
-
-
-
-
-
     }
-
-
-
-
-
 }
